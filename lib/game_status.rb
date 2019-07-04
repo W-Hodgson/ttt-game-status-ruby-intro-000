8a2fdeea7b8604a29aee36e7abaeab2e8b5b3ab6
Will Hodgson <will.hodgson@current-rms.com>
2019-07-04 15:11:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,11 +16,13 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |combination|
+  winning_array = WIN_COMBINATIONS.each do |combination|
     x_victory = combination[0] == "X" && combination[1] == "X" && combination[2] == "X"
     o_victory = combination[0] == "O" && combination[1] == "O" && combination[2] == "O"
     if x_victory || o_victory
       return combination
+    else
+      falsey
     end
   end
 end
