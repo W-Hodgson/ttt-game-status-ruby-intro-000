1ae8d806f32a58e8c592d85990b96d71401f9e6b
Will Hodgson <will.hodgson@current-rms.com>
2019-07-04 15:26:21 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,8 +17,12 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   winning_array = WIN_COMBINATIONS.select do |combination|
-    x_victory = board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X"
-    o_victory = board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
+    x_victory = combination.all? do |index|
+      board[index] = "X"
+    end
+    o_victory = combination.all? do |index|
+      board[index] = "O"
+    end
     if x_victory || o_victory
       combination
     else
@@ -32,17 +36,6 @@ def won?(board)
   end
 end
 
-# def won?(board)
-#   winning_array = WIN_COMBINATIONS.select do |combination|
-#     if position_taken?(board, combination[0]) && position_taken?(board, combination[1]) && position_taken?(board, combination[2])
-#       combination
-#     else
-#       false
-#     end
-#   end
-#   if winning_array != [] and winning_array.length == 1
-#     winning_array[0]
-#   else
-#     false
-#   end
-# end
+def full?(board)
+
+end
