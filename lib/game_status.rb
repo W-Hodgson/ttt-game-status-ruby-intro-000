067162422bf36fe39e43ca3875ed6544efb789a5
Will Hodgson <will.hodgson@current-rms.com>
2019-07-04 14:45:07 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -18,7 +18,7 @@ WIN_COMBINATIONS = [
 def won?(board)
   winning_array = WIN_COMBINATIONS.select do |combination|
     if position_taken?(board, combination[0]) && position_taken?(board, combination[1]) && position_taken?(board, combination[2])
-      true
+      return combination
     else
       false
     end
