4f78c0b2ce261fde53255e4f8c2961eecdb8d9ee
Will Hodgson <will.hodgson@current-rms.com>
2019-07-04 13:56:03 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,13 +17,9 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |combination|
-    win_index_1 = combination[0]
-    win_index_2 = combination[1]
-    win_index_3 = combination[2]
-    if board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X"
-      puts combination
-    else
-      nil
+    combination.all? do |position|
+      if position_taken?(board, position)
+      end
     end
   end
 end
